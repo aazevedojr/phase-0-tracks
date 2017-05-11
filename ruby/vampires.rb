8133830466aa6c711dcf606a2f8964b4bcd25081
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-05-11 16:47:28 -0700

    Add data processing

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,11 +3,31 @@
 puts "What is your name?"
 name = gets.chomp
 puts  "How old are you?"
-age = gets.chomp
+age = gets.to_i
+puts "What year is it?"
+year = gets.to_i
 puts "In which year were you born?"
-birth = gets.chomp
-puts "Would you like some garlic bread?"
-garlic_bread = gets.chomp
-puts "Would you like to enroll in the company’s health insurance?"
-health_insurance = gets.chomp
+birth = gets.to_i
+puts "Would you like some garlic bread? (Y/N)"
+garlic_bread = gets.chomp.downcase
+puts "Would you like to enroll in the company’s health insurance? (Y/N)"
+health_insurance = gets.chomp.downcase
 
+
+# Data Processing
+
+actual_age = year - birth
+
+if garlic_bread == y
+  garlic_bread = TRUE
+else
+  garlic_bread = FALSE
+end
+
+if health_insurance == y
+  health_insurance = TRUE
+else
+  health_insurance = FALSE
+end
+
+# Data Analysis
