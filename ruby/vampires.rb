470d1133576703d22304593e03b673e2e4e84d82
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-05-11 16:16:10 -0700

    Release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,13 @@
+# Gathering Data
+
+puts "What is your name?"
+name = gets.chomp
+puts  "How old are you?"
+age = gets.chomp
+puts "In which year were you born?"
+birth = gets.chomp
+puts "Would you like some garlic bread?"
+garlic_bread = gets.chomp
+puts "Would you like to enroll in the company’s health insurance?"
+health_insurance = gets.chomp
+
