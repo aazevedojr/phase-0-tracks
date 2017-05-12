eecbe4d68ef1270a3f6e37399d1c51904bfd344a
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-05-11 17:45:23 -0700

    If results based on first condition met

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -8,26 +8,62 @@ puts "What year is it?"
 year = gets.to_i
 puts "In which year were you born?"
 birth = gets.to_i
+puts "Has your birthday already passed this year? (Y/N/Today)"
+birthday = gets.chomp.upcase
 puts "Would you like some garlic bread? (Y/N)"
-garlic_bread = gets.chomp.downcase
+garlic_bread = gets.chomp.upcase
 puts "Would you like to enroll in the company’s health insurance? (Y/N)"
-health_insurance = gets.chomp.downcase
+health_insurance = gets.chomp.upcase
 
 
 # Data Processing
 
-actual_age = year - birth
+#1 birthday to boolean:
+if (birthday == "Y") || (birthday == "TODAY")
+  birthday = TRUE
+else
+  birthday = FALSE
+end
 
-if garlic_bread == y
+#2 calculating the employee's actual age:
+if birthday
+  actual_age = year - birth
+else
+  actual_age = year - birth - 1
+end
+
+#3 age to boolean:
+if actual_age == age
+  age = TRUE
+else
+  age = FALSE
+end
+
+#4 garlic_bread to boolean:
+if garlic_bread == "Y"
   garlic_bread = TRUE
 else
   garlic_bread = FALSE
 end
 
-if health_insurance == y
+#5 health_insurance to boolean:
+if health_insurance == "Y"
   health_insurance = TRUE
 else
   health_insurance = FALSE
 end
 
+
 # Data Analysis
+
+if age && (garlic_bread || health_insurance)
+  puts "Probably not a vampire."
+elsif !age && (!garlic_bread || !health_insurance)
+  puts "Probably a vampire."
+elsif !age && !garlic_bread && !health_insurance
+  puts "Almost certainly a vampire."
+elsif (name == "Drake Cula") || (name == "Tu Fang")
+  puts "Definitely a vampire."
+else
+  puts "Results inconclusive."
+end
