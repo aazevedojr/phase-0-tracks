c7982b65beaad007f4da7ac4be4a6853dbd1a21a
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-05-13 11:51:47 -0700

    Minor text fixes

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -50,21 +50,17 @@ end
 # In ruby, it's possible to have nested method calls.
 # It runs the inner most method which will be used as the argument for the outer method.
 
-  puts "Would you like to encrypt or decrypt a password?"
+puts "Would you like to encrypt or decrypt a password?"
+answer = gets.chomp
+until answer == "encrypt" || answer == "decrypt"
+  puts "Please enter either 'encrypt' or 'decrypt'."
   answer = gets.chomp
-  until answer == "encrypt" || answer == "decrypt"
-    puts "Please enter either 'encrypt' or 'decrypt'."
-    answer = gets.chomp
-  end
-  puts "Please enter the password."
-  word = gets.chomp
+end
+puts "Please enter the password."
+word = gets.chomp
 
 if answer == "encrypt"
   puts encrypt(word)
-elsif answer == "decrypt"
-  puts decrypt(word)
 else
-  puts "Sorry, I didn't understand that."
+  puts decrypt(word)
 end
-
-    
