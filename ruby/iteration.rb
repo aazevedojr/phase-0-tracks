2874b59decdeb240246715a77d7f178ec061d09c
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-05-19 20:36:28 -0700

    Add hash.map approach

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -47,3 +47,19 @@ end
 puts "Pick one:"
 p starters_array
 puts "There's no need to choose wisely this time, really..."
+
+# hash.map Version:
+
+puts "-- Green Version --"
+
+glitchy_hash = []
+
+starters_hash.map do |element, pokemon|
+  glitchy_hash << pokemon.next.swapcase.delete("A")
+end
+
+puts "Pick one:"
+p glitchy_hash
+puts "Pokemon Green has only ever been released in Japan,
+so it's only natural that the starter's names are rendered incorrectly.
+Sorry!"
