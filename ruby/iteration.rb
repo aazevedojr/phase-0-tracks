e295cde8c7e6ca4965b7402174a2c08344ff464c
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-05-20 17:16:25 -0700

    Release 2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -63,3 +63,53 @@ p glitchy_array
 puts "Pokemon Green has only ever been released in Japan,
 so it's only natural that the starter's names are rendered incorrectly.
 Sorry!"
+
+
+# Release 2: Use the Documentation
+
+puts "-- Let's get this party started! --"
+
+shopping_list = ["red cups", "beer", "nachos", "cookies", "balloons"]
+
+party_playlist = {
+  rock: "Metallica",
+  pop: "Lady Gaga",
+  rap: "Macklemore",
+  funk: "MC Mayara",
+  electro: "Daft Punk"
+}
+
+puts "Party plans:"
+p shopping_list
+p party_playlist
+
+# Iterate through the items, deleting any that meet a certain condition:
+
+puts "Partying on a budget:"
+p shopping_list.reject { |item| item.length > 6 }
+p party_playlist.reject { |genre, artist| genre == :rap || artist[0] != "M" }
+
+# Filter a data structure for only items that do satisfy a certain condition:
+
+puts "Alcoholics' party:"
+p shopping_list.select { |item| item[0] == "b" }
+p party_playlist.select { |genre, artist| genre == :funk }
+
+# Filter a data structure for only items satisfying a certain condition (other):
+
+puts "Picky's Party"
+
+picky_list = shopping_list
+picky_playlist = party_playlist
+
+picky_list.keep_if { |item| item.length < 8 }
+picky_playlist.keep_if { |genre, artist| genre != :electro && artist != "Lady Gaga" }
+
+p picky_list
+p picky_playlist
+
+# Remove items from a data structure until the condition in the block evaluates to false, then stops:
+
+puts "Superstitious' Party:"
+p shopping_list.drop_while { |item| item.length > 5 }
+p party_playlist.drop_while { |genre, artist| artist.length == 9 }
