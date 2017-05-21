92afde1a21f53ffa05f696e1ada0bb28d1438bae
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-05-20 19:03:27 -0700

    Create Magic Land

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,24 @@
+magic_land = {
+  unicorn_valley: {
+    name: "Unicorn Valley",
+    dwellers: ["Charlie", "Pinky", "Blu", "Liopleurodon"],
+    info: {dangerous: true, evil: nil, magical_tier: 6}
+  },
+  fairy_lake: {
+    name: "Fairy Lake",
+    dwellers: ["Tooth", "Santa", "Godmother", "Queen"],
+    info: {dangerous: false, evil: false, magical_tier: 8}
+  },
+  troll_cave: {
+    name: "Troll Cave",
+    dwellers: ["Internet", "Meme", "Bowser", "Bowser Jr."],
+    info: {dangerous: true, evil: true, magical_tier: 2}
+  },
+  dragon_dungeon: {
+    name: "Dragon Dungeon",
+    dwellers: ["Luck", "Dragonite", "Drogon", "Rhaegal", "Viserion"],
+    info: {dangerous: true, evil: false, magical_tier: 4}
+  }
+}
+
+p magic_land
