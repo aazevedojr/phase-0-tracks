34f2aa470e243e253c29b95d42d40aea70422138
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-05-21 15:45:38 -0700

    Remove redundant argument for advance_letters

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -19,40 +19,13 @@ swapped_name = swap_name(real_name)
 
 #p swapped_name
 
-#def advance_vowel(name)
-#  vowels = ['a','e','i','o','u']
-#  name_letters = name.downcase.chars
-#  p name_letters
-#  name_vowels = name_letters & vowels
-#  p name_vowels
-#  advanced_vowels = name_vowels.map {|vowel| vowel = vowels[vowels.index(vowel) + 1]}
-#  p advanced_vowels
-#  name_letters.map! do |letter|
-#    Not looking good... D:
-#  end
-#  name_letters.join(' ')
-#end
-
-#vowel_advanced_name = advance_vowel(real_name)
-
-# "Dictionary" Approach:
-
-# alphabet = 'abcdefghijklmnopqrstuvwxyz'.chars
-# vowels = ['a','e','i','o','u']
-# consonants = alphabet - vowels
-# advanced_vowels = vowels.map do |vowel|
-#   advanced_index = (vowels.index(vowel) + 1) % vowels.length
-#   vowel = vowels[advanced_index]
-# end
-# p advanced_vowels
-
 alphabet = 'abcdefghijklmnopqrstuvwxyz'.chars
 vowels = ['a','e','i','o','u']
 consonants = alphabet - vowels
 
-# This method takes a string and an array as arguments and returns an array:
+# This method takes an array as arguments and returns an array:
 
-def advance_letters(word, letters)
+def advance_letters(letters)
   advanced_letters = letters.map do |letter|
     advanced_index = (letters.index(letter) + 1) % letters.length
     letter = letters[advanced_index]
@@ -60,12 +33,14 @@ def advance_letters(word, letters)
   advanced_letters
 end
 
-replacement_vowels = advance_letters(real_name, vowels)
+replacement_vowels = advance_letters(vowels)
+replacement_consonants = advance_letters(consonants)
+p replacement_consonants
 
 # This method takes a string and two arrays of same length as arguments and returns a string:
 
 def substitute_letters(word, letters_to_replace, replacement_letters)
-  replaced_letters = word.chars.map do |letter|
+  replaced_letters = word.downcase.chars.map do |letter|
     if letters_to_replace.include? letter
       letter = replacement_letters[letters_to_replace.index(letter)]
     else
@@ -76,4 +51,5 @@ def substitute_letters(word, letters_to_replace, replacement_letters)
 end
 
 p real_name
-p substitute_letters(real_name, vowels, replacement_vowels)
+name_substituted_vowels = substitute_letters(swapped_name, vowels, replacement_vowels)
+p substitute_letters(name_substituted_vowels, consonants, replacement_consonants)
