91f086d83f631252c7bfa7075c7ab48c703a1e7d
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-05-21 14:27:05 -0700

    Fix edge cases

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -19,15 +19,29 @@ swapped_name = swap_name(real_name)
 
 #p swapped_name
 
-def advance_vowel(name)
-  vowels = ['a','e','i','o','u']
-  name_letters = name.downcase.chars
-  name_vowels = name_letters & vowels
-  advanced_vowels = name_vowels.map {|vowel| vowel = vowels[vowels.index(vowel) + 1]}
-  name_letters.map! do |letter|
-    #Stopped here.
-  end
-  name_letters.join(' ')
-end
+#def advance_vowel(name)
+#  vowels = ['a','e','i','o','u']
+#  name_letters = name.downcase.chars
+#  p name_letters
+#  name_vowels = name_letters & vowels
+#  p name_vowels
+#  advanced_vowels = name_vowels.map {|vowel| vowel = vowels[vowels.index(vowel) + 1]}
+#  p advanced_vowels
+#  name_letters.map! do |letter|
+#    Not looking good... D:
+#  end
+#  name_letters.join(' ')
+#end
 
-vowel_advanced_name = advance_vowel(real_name)
+#vowel_advanced_name = advance_vowel(real_name)
+
+# "Dictionary" Approach:
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'.chars
+vowels = ['a','e','i','o','u']
+consonants = alphabet - vowels
+advanced_vowels = vowels.map do |vowel|
+  advanced_index = (vowels.index(vowel) + 1) % vowels.length
+  vowel = vowels[advanced_index]
+end
+p advanced_vowels
