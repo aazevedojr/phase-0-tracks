b743aa6af7b1b004f9037fea4e1248cff2464a01
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-05-21 20:09:18 -0700

    Add fibonacci numbers method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,5 @@
+# Simple Search:
+
 def search_array(array, integer)
   verify = []
   array.each do |number|
@@ -13,8 +15,26 @@ def search_array(array, integer)
   end
 end
 
-arr = [42, 89, 23, 1]
+#arr = [42, 89, 23, 1]
 
-p search_array(arr, 1)
-p search_array(arr, 23)
-p search_array(arr, 24)
+#p search_array(arr, 1)
+#p search_array(arr, 23)
+#p search_array(arr, 24)
+
+
+# Fibonacci Numbers:
+
+def fib(terms)
+  sequence = [0, 1]
+  for i in 1..terms
+    sequence << sequence[i] + sequence[i-1]
+  end
+  sequence.delete_at(-1)
+  sequence.delete_at(-1)
+  sequence
+end
+
+p fib(0)
+p fib(1)
+p fib(6)
+p fib(100).last
