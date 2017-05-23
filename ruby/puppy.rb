4e77f02cb0c7058fcfce8a98c61ad9f8f56e82d0
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-05-23 16:44:59 -0700

    Introduction to Classes

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# CLASS NAME
+#   Puppy
+# 
+# CHARACTERISTICS
+#   Cute: yes
+#   Disciplined: no
+#   Breed: varies
+#   Name: varies
+#   Fur Color: varies
+#   Age: less than 1 year
+# 
+#   BEHAVIOR
+#   Bite
+#   Bark
+#   Sleep
