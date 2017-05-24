dae9ffaf28f95e1fc042a5d46b6e324d93559e2a
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-05-23 19:18:47 -0700

    Finish hash drills

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -113,14 +113,27 @@ puts output
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000. Do not use any special built-in methods.
 
-extinct_animals.keep_if {|animal, year| year < 2000}
+#extinct_animals.keep_if {|animal, year| year < 2000}
 
+extinct_animals.map do |animal, year|
+  if year >= 2000
+    extinct_animals.delete(animal)
+  end
+end
+
+p extinct_animals
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # so they accurately reflect what year the animal went extinct.
 # Do not use any special built-in methods.
 # ----
+
+extinct_animals.map do |animal, year|
+  extinct_animals[animal] = (year - 3)
+end
+
+p extinct_animals
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
 # Check if they're included in extinct_animals, one by one:
@@ -130,8 +143,23 @@ extinct_animals.keep_if {|animal, year| year < 2000}
 # Do not use any special built-in methods.
 # ----
 
+animals_to_be_checked = ["Andean Cat", "Dodo", "Saiga Antelope"]
+
+checked_animals = {}
+
+p animals_to_be_checked
+
+animals_to_be_checked.map do |animal_to_check|
+  checked_animals[animal_to_check] = extinct_animals.keys.include? animal_to_check
+end
+
+p checked_animals
+
+
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
 # Find the built-in method that helps you accomplish this in the Ruby documentation
 # for Hashes.
 # ----
+
+p extinct_animals.rassoc(extinct_animals["Passenger Pigeon"])
