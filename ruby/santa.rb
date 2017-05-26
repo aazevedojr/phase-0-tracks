8fb39a53bbcc52fef3a1bc1c577de95dd624bef0
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-05-25 19:34:49 -0700

    Add default values

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,10 +1,11 @@
 class Santa
-  def initialize(name, gender, ethnicity, age = 0)
+  def initialize(name, gender, ethnicity)
+    puts "Initializing Santa instance for #{name} (#{gender}, #{ethnicity})..."
     @name = name
     @gender = gender
     @ethnicity = ethnicity
-    @age = age
-    puts "Initializing Santa instance for #{@name}: #{@gender}, #{@ethnicity}, #{@age} years old."
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
@@ -14,6 +15,6 @@ class Santa
   end
 end
 
-dad_xmas = Santa.new("St. Nick", "Male", "White", 1400)
+dad_xmas = Santa.new("St. Nick", "male", "white")
 dad_xmas.speak
 dad_xmas.eat_milk_and_cookies("chocolate chip", "hazelnut")
