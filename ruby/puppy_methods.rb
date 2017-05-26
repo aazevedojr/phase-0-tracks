58ac904a50edc7d9491b8233eef7ec371f79ae28
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-05-26 13:21:59 -0700

    Release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,32 @@ class Puppy
     toy
   end
 
+  def speak(number)
+    number.times do
+      puts "Woof"
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    p human_years * 7
+  end
+
+  def stay(seconds)
+    seconds.times do |time|
+      puts "Stayed for #{time + 1} seconds"
+    end
+  end
+
 end
 
+fido = Puppy.new
+
+fido.fetch("ball")
+fido.speak(3)
+fido.roll_over
+fido.dog_years(3)
+fido.stay(5)
