a01ff7c397c43e09843f5f4678a6700bebb07c25
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-05-25 20:10:51 -0700

    Add random Santa generator

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -8,13 +8,21 @@ class Santa
     @age = 0
   end
   def speak
-    puts "Ho, ho, ho! Haaaappy holidays!"
+    puts "#{@name} says: Ho, ho, ho! Haaaappy holidays!"
   end
   def eat_milk_and_cookies(*cookie_flavors)
-    cookie_flavors.each {|flavor| puts "That was a good #{flavor} cookie!"}
+    cookie_flavors.each {|flavor| puts "#{@name} ate a #{flavor} cookie with milk!"}
   end
 end
 
-dad_xmas = Santa.new("St. Nick", "male", "white")
-dad_xmas.speak
-dad_xmas.eat_milk_and_cookies("chocolate chip", "hazelnut")
+# dad_xmas = Santa.new("St. Nick", "male", "white")
+# dad_xmas.speak
+# dad_xmas.eat_milk_and_cookies("chocolate chip", "hazelnut")
+
+santas = []
+santa_names = ["Father Christmas", "Saint Nicholas", "Santa Claus", "Kris Kringle", "Noel", "The Grinch"]
+example_genders = ["agender", "cis-female", "bigender", "cis-male", "gender fluid", "trans-male", "trans-female", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Asian", "native American", "Mystical Creature", "other", "N/A"]
+santa_names.length.times do |i|
+  santas << Santa.new(santa_names[i], example_genders[rand(example_genders.length)], example_ethnicities[rand(example_ethnicities.length)])
+end
