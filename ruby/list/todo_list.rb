5c154ed7cd50705a94b90e027cb135c8af1e120a
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-05-26 18:38:55 -0700

    Initial commit

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,21 @@
+class TodoList
+  def initialize(chores)
+    chores
+  end
+
+  def get_items
+    #
+  end
+
+  def add_item
+    #
+  end
+
+  def delete_item
+    #
+  end
+
+  def get_item(i)
+    #
+  end
+end
