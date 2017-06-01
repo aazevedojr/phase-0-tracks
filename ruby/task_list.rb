55ab9ae1692fb981e53f383964159af6f5c6e681
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-06-01 14:54:57 -0700

    Add location method

--- ruby/task_list.rb
+++ ruby/task_list.rb
@@ -1,9 +1,28 @@
 class TaskList
-  attr_reader :owner
+  attr_reader :owner, :task_locations
   attr_accessor :due_date
 
   def initialize(owner, due_date)
     @owner = owner
     @due_date = due_date
+    @task_locations = {}
+  end
+
+  def add_location(location, *task)
+    if !task_locations.include?(location)
+      task_locations[location] = task
+    else
+      puts "#{location} location has already been assigned."
+    end
   end
 end
+
+#Test Code
+
+tasks = TaskList.new('Guto', 'Jun 03')
+
+tasks.add_location('home', 'pay rent', 'take the trash out')
+
+tasks.add_location('home', 'vacuum the carpet')
+
+p tasks
