106149bbf1e5fb975b8baa5cbd75f87ed3c247b7
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-06-01 15:19:50 -0700

    Add task method

--- ruby/task_list.rb
+++ ruby/task_list.rb
@@ -15,6 +15,18 @@ class TaskList
       puts "#{location} location has already been assigned."
     end
   end
+
+  def add_task(task, location)
+    if task_locations.include?(location)
+      if !task_locations[location].include?(task)
+        task_locations[location] << task
+      else
+        puts "#{task} task has already been assigned."
+      end
+    else
+      puts "#{location} location has not yet been assigned."
+    end
+  end
 end
 
 #Test Code
@@ -23,6 +35,6 @@ tasks = TaskList.new('Guto', 'Jun 03')
 
 tasks.add_location('home', 'pay rent', 'take the trash out')
 
-tasks.add_location('home', 'vacuum the carpet')
+tasks.add_task('vacuum the carpet', 'home')
 
 p tasks
