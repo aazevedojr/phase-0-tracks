5c06b2a6d8cd06ccc27fa63a30ef137116fc3ec0
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-06-01 16:16:42 -0700

    Minor text fixes

--- ruby/task_list.rb
+++ ruby/task_list.rb
@@ -11,8 +11,9 @@ class TaskList
   def add_location(location, *task)
     if !task_locations.include?(location)
       task_locations[location] = task
+      puts "Add '#{location}' location to list."
     else
-      puts "#{location} location has already been assigned."
+      puts "'#{location}' location has already been assigned."
     end
   end
 
@@ -20,8 +21,9 @@ class TaskList
     if task_locations.include?(location)
       if !task_locations[location].include?(task)
         task_locations[location] << task
+        puts "Add '#{task}' to list."
       else
-        puts "#{task} task has already been assigned."
+        puts "'#{task}' task has already been assigned."
       end
     else
       add_location(location, task)
@@ -31,13 +33,14 @@ class TaskList
   def complete_task(task, location)
     if task_locations[location].include?(task)
       task_locations[location].delete(task)
-      puts "Crossed #{task} task off!"
+      puts "Crossed '#{task}' task off!"
     else
-      puts "You didn't have the task #{task} assigned to #{location} location."
+      puts "You didn't have the task '#{task}' assigned to #{location} location."
     end
   end
 
   def to_s
+    puts "#{owner}'s TaskList:"
     task_locations.each do |location, tasks|
       puts "At #{location}:"
       tasks.each do |task|
@@ -49,13 +52,17 @@ class TaskList
   end
 
   def is_past_due?(today)
-    weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    if weekdays.index(today) > weekdays.index(due_date)
-      puts "Your tasks are past due!"
-    elsif weekdays.index(today) < weekdays.index(due_date)
-      puts "You have #{weekdays.index(due_date) - weekdays.index(today)} days to finish your tasks."
+    if task_locations.values.join('') != ""
+      weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+      if weekdays.index(today) > weekdays.index(due_date)
+        puts "Your tasks are past due, #{owner}!"
+      elsif weekdays.index(today) < weekdays.index(due_date)
+        puts "You have #{weekdays.index(due_date) - weekdays.index(today)} days to finish your tasks, #{owner}."
+      else
+        puts "Your tasks are due today, #{owner}!"
+      end
     else
-      puts "Your tasks are due today!"
+      puts "#{owner}, you've finished all your tasks!"
     end
   end
 end
@@ -72,3 +79,11 @@ tasks.add_task('vacuum the carpet', 'home')
 tasks.is_past_due?('Thursday')
 tasks.is_past_due?('Friday')
 tasks.is_past_due?('Saturday')
+
+notasks = TaskList.new('Ali', 'Friday')
+notasks.add_location('home', 'sleep')
+notasks.complete_task('sleep', 'home')
+
+notasks.is_past_due?('Thursday')
+notasks.is_past_due?('Friday')
+notasks.is_past_due?('Saturday')
