2236850a319b736acbe76f1492ad418851ee6d5f
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-05-31 21:43:14 -0700

    Iterate through every state

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,14 +1,15 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge with: Mitchell Hendee.
+# We spent 1:30 hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# Require relative allows the file to interact with other files in the same directory
+# The difference between it and 'require' needs a definite path to the new file.
 require_relative 'state_data'
 
 class VirusPredictor
+  attr_reader :state, :population, :population_density
 
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
@@ -17,24 +18,24 @@ class VirusPredictor
   end
 
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths(population_density, population, state)
+    speed_of_spread(population_density, state)
   end
 
   private
 
   def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+    if population_density >= 200
+      number_of_deaths = (population * 0.4).floor
+    elsif population_density >= 150
+      number_of_deaths = (population * 0.3).floor
+    elsif population_density >= 100
+      number_of_deaths = (population * 0.2).floor
+    elsif population_density >= 50
+      number_of_deaths = (population * 0.1).floor
     else
-      number_of_deaths = (@population * 0.05).floor
+      number_of_deaths = (population * 0.05).floor
     end
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
@@ -46,13 +47,13 @@ class VirusPredictor
     # by additional factors we haven't added into this functionality.
     speed = 0.0
 
-    if @population_density >= 200
+    if population_density >= 200
       speed += 0.5
-    elsif @population_density >= 150
+    elsif population_density >= 150
       speed += 1
-    elsif @population_density >= 100
+    elsif population_density >= 100
       speed += 1.5
-    elsif @population_density >= 50
+    elsif population_density >= 50
       speed += 2
     else
       speed += 2.5
@@ -64,24 +65,16 @@ class VirusPredictor
 
 end
 
+
 #=======================================================================
 
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
-
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
-
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
-
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
-
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
-
+STATE_DATA.each do |state, data|
+  report = VirusPredictor.new(state, STATE_DATA[state][:population_density], STATE_DATA[state][:population])
+  report.virus_effects
+end
 
 #=======================================================================
 # Reflection Section
