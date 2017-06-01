edb157db5d5256b0a5e6cfd98382e9350b96ae55
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-06-01 00:31:29 -0700

    Improve user interface and input list

--- ruby/house/user_interface.rb
+++ ruby/house/user_interface.rb
@@ -4,7 +4,6 @@ puts 'House Builder Alpha'.upcase
 puts 'Enter the house address:'
 
 house = House.new(gets.chomp)
-puts house
 
 puts "Enter a name for the first room:"
 room_name = gets.chomp
@@ -14,5 +13,15 @@ puts "Enter the length of the room (m):"
 room_length = gets.to_i
 
 house.add_room(room_name, room_width, room_length)
+p house.rooms[0]
 
+puts "Add piece of furniture to #{room_name}:"
+item_name = gets.chomp
+puts "What is it made of?"
+item_material = gets.chomp
+puts "How much does it cost (¥)?"
+item_price = gets.to_i
 
+item = house.rooms[0].add_item(item_name, item_material, item_price)
+
+puts house
