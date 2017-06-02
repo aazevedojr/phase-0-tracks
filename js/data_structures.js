dd9327db9f88fb68c30af7cbb5777ea1fa3faa93
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-06-02 15:06:49 -0700

    Make both arrays the same length

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,11 +1,9 @@
 var horse_colors = ["vantablack", "forest green", "true blue", "mellow yellow", "purple haze", "lava red"]
 var horse_names = ["BoJack Horseman", "Secretariat", "Tiffany", "Silverlight"]
 
-console.log(horse_colors)
-console.log(horse_names)
-
 horse_colors.push("TiO2 white")
-horse_names.push("Potranca")
+horse_names.push("Potranca", "James Baxter")
+horse_colors.splice(4,1) //It turns out 'purple haze' is not a color at all!
 
 console.log(horse_colors)
 console.log(horse_names)
