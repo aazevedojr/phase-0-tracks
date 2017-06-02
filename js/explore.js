414e5792eb9835880a59f413f324e28a9884c768
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-06-02 12:14:46 -0700

    Add string reverting loop

--- js/explore.js
+++ js/explore.js
@@ -7,3 +7,12 @@
 // Stop when new position = old position OR
 // if the character has already been moved.
 // Print the string.
+
+var str = "DevBootcamp"
+var rts = ""
+
+for (var i = str.length - 1; i >= 0; i--) {
+  rts += str[i]
+}
+
+console.log(rts)
