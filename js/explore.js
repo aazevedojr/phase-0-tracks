95f9c3ae5895828957de9913db1b9eceb8384c37
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-06-01 22:46:29 -0700

    Initial commit

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// REVERSING A STRING
+
+// Declare a string.
+// Iterate from the first character to the last, doing:
+//  Move character at position 0 to position -1,
+//  Move character at position 1 to position -2...
+// Stop when new position = old position OR
+// if the character has already been moved.
+// Print the string.
