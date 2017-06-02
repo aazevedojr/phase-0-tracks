d09b02bb0e318ffad8c1696758aff6d31eeabf68
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-06-02 14:33:25 -0700

    Create two arrays

---
+++ js/data_structures.js
@@ -0,0 +1,11 @@
+var horse_colors = ["vantablack", "forest green", "true blue", "mellow yellow", "purple haze", "lava red"]
+var horse_names = ["BoJack Horseman", "Secretariat", "Tiffany", "Silverlight"]
+
+console.log(horse_colors)
+console.log(horse_names)
+
+horse_colors.push("TiO2 white")
+horse_names.push("Potranca")
+
+console.log(horse_colors)
+console.log(horse_names)
