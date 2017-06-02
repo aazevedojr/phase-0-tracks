fe00d5616f0f7be3073adabab44920a99c57675e
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-06-02 16:53:16 -0700

    Add two new cars

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,5 @@
+// HORSES
+
 var horseColors = ["vantablack", "forest green", "true blue", "mellow yellow", "purple haze", "lava red"]
 var horseNames = ["BoJack Horseman", "Secretariat", "Tiffany", "James Baxter"]
 
@@ -15,3 +17,35 @@ for (var i = 0; i <= horseNames.length - 1; i++) {
 }
 
 console.log(horseInfo)
+
+
+// CARS
+
+function  Car(brand, selfDriving, color, price) {
+  this.brand = brand;
+  this.selfDriving = selfDriving;
+  this.color = color;
+  this.price = price;
+  this.honk = function(times) {
+    console.log('Beeb! '.repeat(times))
+  };
+  this.driveMeHome = function() {
+    if (this.selfDriving) {
+      console.log('You have arrived home safely.')
+    } else {
+      console.log('Better call an uber!')
+    }
+  }
+}
+
+var car = new Car('Tesla', true, 'vantablack', 1000000)
+
+console.log(car.color + ' ' + car.brand + ':')
+car.honk(3)
+car.driveMeHome()
+
+var beetle = new Car('Volkswagen', false, 'TiO2 white', 5000)
+
+console.log(beetle.color + ' ' + beetle.brand + ':')
+beetle.honk(2)
+beetle.driveMeHome()
