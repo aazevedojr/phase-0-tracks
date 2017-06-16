9ce46686e17d98416f80c52f7415561f06196cbb
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-06-15 23:56:42 -0700

    Make logo different each time page is loaded

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,1 +1,22 @@
 console.log("The script is running!")
+
+//function changePicture(event) {
+//  console.log("Clicked on picture!")
+//
+//}
+
+var drawing = document.getElementById("logo");
+
+var drawings = [
+  "ariados",
+  "clamperl",
+  "ekans",
+  "him",
+  "ice_king",
+  "idk",
+  "lugia",
+  "owl",
+  "zygarde"
+]
+
+drawing.src = "drawings/" + drawings[Math.floor((Math.random() * drawings.length))] + ".jpg";
